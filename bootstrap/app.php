beeb6762847233172440110d953805371bd57b30
Alan Mora <alanmora011299@gmail.com>
2026-06-10 21:27:25 -0400

    fix: add new routes for the API format and command application to dynamically generate services.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,12 +4,23 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Console\Commands\Services;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withCommands([
+        Services::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function() {
+            Route::prefix('api/v1/products')
+                ->namespace('App\Http\Controllers')
+                ->group(base_path('routes/v1/products.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
