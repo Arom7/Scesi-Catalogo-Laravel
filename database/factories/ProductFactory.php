88d36b6ff19343f3b2ef175689acf789954ef69c
Alan Mora <alanmora011299@gmail.com>
2026-06-11 23:35:05 -0400

    feat: add API routes granted during installation, factories for working with fake data and response format of an individual product

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(),
+            'base_price' => $this->faker->randomFloat(2, 10, 1000),
+        ];
+    }
+}
