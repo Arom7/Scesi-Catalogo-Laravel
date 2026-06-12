1131ed1bff9dfbe089c75bdb5c78a01a6201884a
Alan Mora <alanmora011299@gmail.com>
2026-06-11 23:32:00 -0400

    feat: add different requests depending on the method

---
+++ app/Http/Requests/UpdateProductRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'sometimes|required|string|max:255',
+            'description' => 'sometimes|nullable|string',
+            'base_price' => 'sometimes|required|numeric|min:10',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'El nombre es requerido',
+            'name.max' => 'El nombre no puede exceder los 255 caracteres',
+            'description.string' => 'La descripción debe ser un texto',
+            'base_price.required' => 'El precio base es requerido',
+            'base_price.numeric' => 'El precio base debe ser un número',
+            'base_price.min' => 'El precio base debe ser al menos 10',
+        ];
+    }
+}
